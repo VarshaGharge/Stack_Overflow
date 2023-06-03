ccbf5b734228b680b856638ef52582f42e78f22e
Varsha <varsha0507001@gmail.com>
2023-06-03 23:04:46 +0530

    MongoDB connection

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000
 
 
 
-const DATABASE_URL = process.env.CONNECTION_URL
+const DATABASE_URL = "mongodb+srv://admin:admin@stack-overflow-clone-va.fwqyudk.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => {console.log(`server running on the port ${PORT}`)}))
